f37c31d1342b2f5fa55fa026687466279d9f5cd2
Andrii Lishchyna <andrej044@i.ua>
2020-10-13 20:29:58 +0200

    change get element on create elem in brickData

--- Game.js
+++ Game.js
@@ -1,37 +1,40 @@
 const dataGame = {
-  gameObj: [],
-  itemCount: 10,
-  startPositionX: 0,
-  startPositionY: 0,
-  interval: 1000,
-  clearInteger: 0,
+    gameObj: [],
+    itemCount: 10,
+    startPositionX: 0,
+    startPositionY: 0,
+    interval: 1000,
+    clearInteger: 0,
+    board: document.querySelector('.board'),
 };
 
 class Game {
-  constructor() {}
-  resetGame() {
-    clearInterval(dataGame.clearInteger);
-    brickData.stepsY = 0;
-    brickData.stepsX = 0;
-  }
-  startGame() {
-    for (let i = 0; i < 10; i++) {
-      dataGame.gameObj.push(new Brick());
+    constructor() {}
+    resetGame() {
+        clearInterval(dataGame.clearInteger);
+        brickData.stepsY = 0;
+        brickData.stepsX = 0;
     }
-    console.log(dataGame.gameObj);
+    startGame() {
+        for (let i = 0; i < 10; i++) {
+            dataGame.gameObj.push(new Brick(i));
+        }
+        console.log(dataGame.gameObj[1]);
 
-    const elem = new Brick();
-    this.resetGame();
-    elem.controls();
-    elem.brick.style.top = `${dataGame.startPositionY}px`;
-    elem.brick.style.left = `${dataGame.startPositionX}px`;
-    dataGame.clearInteger = setInterval(
-      elem.moveDown.bind(elem),
-      dataGame.interval
-    );
+        // const elem = new Brick();
+        this.resetGame();
+        dataGame.gameObj[1].controls();
+        dataGame.gameObj[1].brick.style.top = `${dataGame.startPositionY}px`;
+        dataGame.gameObj[1].brick.style.left = `${dataGame.startPositionX}px`;
+        dataGame.clearInteger = setInterval(
+            dataGame.gameObj[1].moveDown.bind(dataGame.gameObj[1]),
+            dataGame.interval
+        );
 
-    // elem.controls();
-    // console.log(dataGame.gameObj.push(new Brick))
-    // console.log(dataGame.gameObj)
-  }
+        dataGame.board.appendChild(dataGame.gameObj[2].brick);
+
+        // elem.controls();
+        // console.log(dataGame.gameObj.push(new Brick))
+        // console.log(dataGame.gameObj)
+    }
 }
