5077487d63fc2d67cd6c52b0f691f0d554183101
Andrii Lishchyna <andrej044@i.ua>
2020-12-26 16:34:01 +0100

    initial class Brick

---
+++ Brick.js
@@ -0,0 +1,23 @@
+export class Brick{
+    
+    constructor(){
+        this.brick; 
+       
+    }
+    createBrick(){
+        this.brick = document.createElement('div');
+        this.brick.classList.add('brick');
+        return this.brick;
+    }
+    moveDown(){
+
+    }
+    moveLeft(){
+
+
+    }
+    moveRight(){
+
+    }
+
+}
